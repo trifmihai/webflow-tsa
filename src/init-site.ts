855d77b1a32af2db30ce14b03bd98dc1794816f5
Mike Trif <97960573+trifmihai@users.noreply.github.com>
2026-06-17 17:41:52 +0300

    Initialize benefit experience Rive feature

--- src/init-site.ts
+++ src/init-site.ts
@@ -11,6 +11,7 @@ import { initRelatedArticlesEmptyState } from './components/related-articles';
 import { initResourcesFilters } from './components/resources-filter';
 import { initTeamCards } from './components/team-card';
 // import { initTextReveal } from './components/text-reveal';
+import { initBenefitExperienceRive } from './features/benefit-experience-rive/benefit-experience-rive';
 import { onDomReady } from './utils/dom';
 
 export function initSite(): void {
@@ -26,5 +27,6 @@ export function initSite(): void {
   initBlogToc();
   onDomReady(initHeroAvatarHover);
   onDomReady(initRelatedArticlesEmptyState);
+  onDomReady(initBenefitExperienceRive);
   // onDomReady(initTextReveal);
 }
